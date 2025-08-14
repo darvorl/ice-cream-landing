0e4f91d4dd6f81d7687f714f8c12bdb2cacf4fd4
Dario <valentinorodriguezlozada@gmail.com>
2025-08-13 19:43:14 -0500

    add: feature section

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,29 @@ const App = () => {
           <Button content="Explore Our Vegan Flavors" color="F83D8E" />
         </div>
       </section>
+      <section className="">
+        <div className="py-23 max-w-[1440px] mx-auto px-16 flex gap-18 items-center justify-center">
+          <img
+            className="max-h-[520px]"
+            src="./feature-image.png"
+            alt="ice cream"
+          />
+          <div className="">
+            <h2 className="mb-9 text-[#0F0200] font-[Berkshire_Swash] text-6xl leading-16 max-w-[384px]">
+              Experience the Art of{" "}
+              <span className="text-[#F83D8E]">Gelato</span>
+            </h2>
+            <p className="font-[Archivo] text-[#646464] text-xl mb-5">
+              Handcrafted Flavors with Fresh, Local Ingredients.
+            </p>
+            <p className="font-[Archivo] text-[#646464] text-xl mb-11 max-w-[434px]">
+              Enjoy gelato made with the finest ingredients and traditional
+              techniques.
+            </p>
+            <Button content="See Our Gelato Menu" color="F83D8E" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
